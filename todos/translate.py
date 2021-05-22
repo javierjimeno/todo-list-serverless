f18fb0f56b55093458207410ea5c5f7100491e6e
EC2 Default User <ec2-user@ip-172-31-83-16.ec2.internal>
2021-05-22 06:05:14 +0000

    Añadido endpoint translate

--- todos/translate.py
+++ todos/translate.py
@@ -17,14 +17,10 @@ def getTranslate(event, context):
         }
     )
     result['Item']['text'] = translate.translate_text(Text=result['Item']['text'], SourceLanguageCode="auto", TargetLanguageCode=event['pathParameters']['language'])['TranslatedText']
-    
-    #result = translate.translate_text(Text=result['Item'].get('text'), SourceLanguageCode="auto", TargetLanguageCode=event['pathParameters']['language'])
 
     # create a response
     response = {
         "statusCode": 200,
-        #"body": json.dumps(result.get('TranslatedText'),
-         #                  cls=decimalencoder.DecimalEncoder)
         "body": json.dumps(result['Item'],
                            cls=decimalencoder.DecimalEncoder)
     }
